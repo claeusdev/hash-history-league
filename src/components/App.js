3d53732957a73c83319859079d60521430a530c3
Nana Adjei Manu <claeusdev@outlook.com>
2019-04-15 01:28:36 +0000

    add sidebar to players page

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import Home from './Home'
 import Teams from './Teams'
 import Players from './Players'
 import Navbar from './Nav'
+
 class App extends Component {
   render() {
     return (
     	<Router>
     		<main>
     			<Navbar/>
-			    <Route path="/" exact component={Home} />
-			    <Route path="/players" component={Players} />
-			    <Route path="/teams" component={Teams} />
+
+    			<Switch>
+				    <Route path="/" exact component={Home} />
+				    <Route path="/players" component={Players} />
+				    <Route path="/teams" component={Teams} />
+
+				    <Route render={() => <h1 className="text-center">Four oh four</h1>} />
+			    </Switch>
 		    </main>
     	</Router>
     );
